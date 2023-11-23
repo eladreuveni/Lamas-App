cdd343529743d11245448560e9761d5878096cb8
eladreuveni <elad.reuveni.7@gmail.com>
2023-11-23 14:57:37 +0200

    docs

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import Content from "./components/Content";
 import Footer from "./components/Footer";
 import TopBar from "./components/TopBar";
 import { AppContextProvider } from "./context/AppContext";
-const fetch = require("node-fetch");
-var myHeaders = new fetch.Headers();
-
 
 export default function Home() {
   return (
